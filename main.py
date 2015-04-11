bf4b9cc38324ff449520f7cb83b76898280ae3e3
kristian@purestyle.se <kristian@purestyle.se>
2015-04-11 10:40:36 +0200

    init

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Main():
+    def __init__(self):
+        print "hello"
+
+    def main_loop(self):
+        print "hello im main loop"
+
+if __name__ == '__main__':
+    main = Main()
+    main.main_loop()
