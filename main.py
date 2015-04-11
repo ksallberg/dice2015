75fd6d278e2669bfdf10aab380b2aeb0761aa6bf
kristian@purestyle.se <kristian@purestyle.se>
2015-04-11 10:46:23 +0200

    init

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import pygame
 
 class Main():
+
+    clock    = None
+    screen   = None
+
     def __init__(self):
         print "hello"
+        pygame.init()
+        pygame.display.set_caption('Wallstreet Tycoon')
+        self.screen = pygame.display.set_mode((500,
+                                               300))
+        self.clock = pygame.time.Clock()
 
     def main_loop(self):
-        print "hello im main loop"
+        while True:
+            print "hello im main loop"
+            self.screen.fill((0,0,0))
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     main = Main()
