febeec96ab99d39057b53f32d89b284f0b5255d6
Didriksson <mattias.didriksson@gmail.com>
2015-04-11 12:19:05 +0200

    Plane och missile är egna klarrer

--- energy_defence_systems/main.py
+++ energy_defence_systems/main.py
@@ -3,8 +3,6 @@ import os.path
 from pygame.locals import *
 from terrain import Terrain
 
-
-
 class Plane():
 	def __init__(self, sprite, initialPosition, deltaMovement):
 		self.sprite = sprite
@@ -25,8 +23,24 @@ class Plane():
 
 		
 class Missile():
-	def __init__(self, sprite):
+	def __init__(self, sprite, initialPosition, deltaMovement):
 		self.sprite = sprite
+		self.x = initialPosition[0]
+		self.y = initialPosition[1]
+		self.deltaMovement = deltaMovement
+		
+	def getSprite(self):
+		return self.sprite
+		
+	def getPosition(self):
+		return (self.x, self.y)
+	
+	def applyForce(self):
+		self.x += self.deltaMovement[0]
+		self.y += self.deltaMovement[1]
+	
+		
+		
 class Main():
 
 	sprites = {}
@@ -34,10 +48,8 @@ class Main():
 	enemyMissiles = []
 	clock	= None
 	screen	= None
-	planePosition = [100,150]
 	deltaMovement = 2
 	terrain = None
-
 	def __init__(self):
 		print "hello"
 		pygame.init()
@@ -50,8 +62,10 @@ class Main():
 		
 		planeSprite = pygame.image.load(os.path.join('sprites','plane.png'))
 		self.plane = Plane(planeSprite, [200,150], 2)
+		self.sprites["myMissile"] = pygame.image.load(os.path.join('sprites','myMissile.png'))
+		self.myMissileMovementSpeed = [5,0]
+
 		
-		self.sprites["missile"] = pygame.image.load(os.path.join('sprites','missile.png'))
 
 	def main_loop(self):
 		while True:
@@ -64,20 +78,24 @@ class Main():
 			if keys[K_DOWN]:
 				self.plane.moveDown()
 			if keys[K_SPACE]:
-				self.myMissiles.append([self.planePosition[0] + self.sprites["missile"].get_width(), self.planePosition[1]])
-			
+				self.myMissiles.append(Missile(self.sprites["myMissile"], self.plane.getPosition(), self.myMissileMovementSpeed))
 			for e in pygame.event.get():
 				continue
-			
 			self.drawSprites()
 			
+			self.applyForcesOfTheWorld()
 			self.terrain.draw(self.screen)
 			pygame.display.flip()
 	
+	
+	def applyForcesOfTheWorld(self):
+		for missile in self.myMissiles:
+			missile.applyForce()
+		
 	def drawSprites(self):
 		self.screen.blit(self.plane.getSprite(), self.plane.getPosition())
 		for missile in self.myMissiles:
-			self.screen.blit(self.sprites["missile"], missile)
+			self.screen.blit(missile.getSprite(), missile.getPosition())
 
 	
 if __name__ == '__main__':
